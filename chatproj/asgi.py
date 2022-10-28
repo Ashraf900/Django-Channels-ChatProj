230b29966dac2b9acfa2332bcac067dfe668c912
suraiyakhan <ashraf9004542268@gmail.com>
2022-10-28 20:03:48 +0530

    first commit

---
+++ chatproj/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for chatproj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from chatapp import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproj.settings')
+
+application = ProtocolTypeRouter({
+    "http":get_asgi_application(),
+    "websocket":AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns),)
+    )
+}
+)
